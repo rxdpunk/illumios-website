6e3bcda5c8074eb2ba0ebfce26ac7bd9cca06178
Sposato <steven.sposato@icloud.com>
2026-04-14 12:46:49 -0400

    Update academia website copy and lead config

---
+++ ghl-config.js
@@ -0,0 +1,10 @@
+window.ILLUMIOS_GHL = Object.assign(
+  {
+    // Paste the HighLevel Inbound Webhook URL from the Academia lead-capture workflow here.
+    // This URL is public if committed in a static site. Rotate it in GHL if needed.
+    inboundWebhookUrl: '',
+    locationId: 'OZuyOTAOTmf8eXnn8n0G',
+    offerSlug: 'illumios-academia'
+  },
+  window.ILLUMIOS_GHL || {}
+);
